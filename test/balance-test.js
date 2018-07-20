1c2359bcf6a6a5984fb39dede36aee3a7cdaab2d
Megan Arellano <ssciolist@gmail.com>
2018-07-20 11:00:39 -0600

    adds use cases, does not resolve errors

--- test/balance-test.js
+++ test/balance-test.js
@@ -5,32 +5,40 @@ const expect = chai.expect;
 const balance = require('../lib/balance')
 
 describe('balance', function() {
-  it('it returns true if the function is balanced', function() {
+  it('it returns true if ()', function() {
     let string = '()';
     expect(balance(string)).to.eql(true)
   });
-  it('it returns true if the function is balanced', function() {
+  it('it returns true if (())', function() {
     let string = '(())';
     expect(balance(string)).to.eql(true)
   });
-  it('it returns true if the function is balanced', function() {
+  it('it returns true if [](){}', function() {
     let string = '[](){}';
     expect(balance(string)).to.eql(true)
   });
-  it('it returns false if the function is unbalanced', function() {
+  it('it returns true if var greeting = { sayHello: hello() }', function() {
+    let string = 'var greeting = { sayHello: hello() }';
+    expect(balance(string)).to.eql(true)
+  });
+  it('it returns false if (', function() {
     let unbalanced_string = '(';
     expect(balance(unbalanced_string)).to.eql(false)
   })
-  it('it returns false if the function is unbalanced', function() {
+  it('it returns false if )(', function() {
     let unbalanced_string = ')(';
     expect(balance(unbalanced_string)).to.eql(false)
   })
-  it('it returns false if the function is unbalanced', function() {
+  it('it returns false if [(]{)}', function() {
     let unbalanced_string = '[(]{)}';
     expect(balance(unbalanced_string)).to.eql(false)
   })
-  it('it returns false if the function is unbalanced', function() {
+  it('it returns false if ][)(}{', function() {
     let unbalanced_string = '][)(}{';
+    expect(balance(unbalanced_string)).to.eql(false)
+  })
+  it('it returns false if var sayGoodbye = function() { goodbye();', function() {
+    let unbalanced_string = 'var sayGoodbye = function() { goodbye();';
     expect(balance(unbalanced_string)).to.eql(false)
   })
 })
